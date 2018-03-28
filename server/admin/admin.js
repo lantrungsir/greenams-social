160c7e32d178ef7685974e25e1dc5ab67e9f8f2f
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-28 22:37:25 +0700

    I am strong

--- server/admin/admin.js
+++ server/admin/admin.js
@@ -1,11 +1,9 @@
 var admin = require("firebase-admin");
+var serviceAccount = require("../../serviceAccount.json")
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://free-schedule.firebaseio.com"
+});
 module.exports= {
     db : admin.database(),
-    verify : function(){
-        var serviceAccount = require("../../serviceAccount.json");
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-            databaseURL: "https://free-schedule.firebaseio.com"
-        });
-    }
 }
