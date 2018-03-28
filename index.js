7b5586c54cb57d3401464b2eacdd9223b5cdf89f
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-28 23:28:14 +0700

    I am strong

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express");
-var app  =express();
+var app  = express();
 var Route = require("./server/config/route.js");
 var Middleware = require("./server/config/middleware.js");
 //config
 Route(app);
 Middleware(app, express);
 //run
-app.listen(process.env.PORT || 1692, function(){
-    console.log("we are on again, GART6520");
+app.set('port',process.env.PORT|| 1692 )
+app.listen(app.get('port'), function(){
+    console.log("we are on again, GART6520 on" + app.get('port'));
 })
