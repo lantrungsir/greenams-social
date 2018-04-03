04c816cca6a03e5dcde5385ec7b335968e16961d
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-03 22:40:18 +0700

    I am strong

--- server/admin/storage.js
+++ server/admin/storage.js
@@ -1,9 +1,7 @@
 var bucket = require("./admin.js").storage().bucket();
-var db = require("./admin.js").database();
-var database = require("./db.js");
+var db = require("./db.js");
 module.exports ={
     uploadFiles : function(num, files, res){
-        new Promise((agree, disagree)=>{
             for(var i = 0;i < files.length ;i++){
                     var file = bucket.file(files[i].originalname);
                     var stream = bucketFile[i].createWriteStream({
@@ -12,42 +10,17 @@ module.exports ={
                         }
                     })
                     stream.on('error', (err)=>{
-                        console.log("err");
+                        console.log(err);
                         res.status(404).send("Fail to upload files")
                     })
                     stream.on("finish", ()=>{
                         console.log("GREAT");
                         console.log(file.name)
-                            console.log(response)
-                            new Promise((resolve,reject)=>{
-                                db.ref("posts/content").once("value", function(snapshot){
-                                    snapshot.forEach(function(post){
-                                        if(post.key.toString() === num.toString()){
-                                            database.pushData("posts/content/"+ post.key +"/files", getPublicUrl(file.name))
-                                            resolve();
-                                        }
-                                    })
-                                })
-                            })
-                            .then(()=>{
-                                console.log("success making file public");
-                                if(i === files.length-1){
-                                    agree();
-                                }
-                            })
-                           .catch((err)=>{
-                                console.log("fail "+err);
-                                disagree();
-                            })
-                        
+                        db.saveData("posts/contents/"+num+"/files", getPublicUrl(file.name))
                     })
                     stream.end(files[i].buffer);
                 }
-            }).then(()=>{
-                res.status(200).send("good")
-            }).catch(()=>{
-                res.status(404).send("fail to upload file, please re-upload")
-            }) 
+            res.status(200).send("good")  
     }
 }
 function getPublicUrl (filename) {
