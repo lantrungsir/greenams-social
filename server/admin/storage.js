b25456f2f9cd7d954401d440d304649988e642d7
Trung <lantrungsir28@gmail.com>
2018-04-04 11:55:32 +0700

    F

--- server/admin/storage.js
+++ server/admin/storage.js
@@ -3,6 +3,8 @@ var db = require("./db.js");
 module.exports ={
     uploadFiles : function(num, files, res){
             for(var i = 0;i < files.length ;i++){
+                new Promise((resolve,reject)=>{
+                    var j =i;
                     var file = bucket.file(files[i].originalname);
                     var stream = file.createWriteStream({
                         metadata:{
@@ -15,12 +17,17 @@ module.exports ={
                     })
                     stream.on("finish", ()=>{
                         console.log("GREAT");
-                        console.log(stream)
+                        console.log(file.name)
                         db.pushData("posts/content/"+num+"/files", getPublicUrl(file.name))
+                        resolve(j);
                     })
                     stream.end(files[i].buffer);
-                }
-            res.status(200).send("good")  
+                }).then(()=>{
+                    if(j === files.length-1){
+                        res.status(200).send("good");
+                    }
+                })
+            }
     }
 }
 function getPublicUrl (filename) {
