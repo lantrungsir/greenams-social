ffac2f1e7e08f6cddabbc2989993316d1ec5dcf9
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-04 18:36:56 +0700

    I am strong

--- server/admin/storage.js
+++ server/admin/storage.js
@@ -42,7 +42,7 @@ module.exports ={
                         agree(result)
                     }
                     else{
-                        continue;
+                        
                     }
                 })
             }
