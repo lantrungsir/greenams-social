f2dbc1d6e6928d9acb48845e5636917c9042d27a
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-05 19:51:51 +0700

    I am strong

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import * as io from "socket.io-client"
 @Injectable()
 export class UpdateService {
-  
-  constructor(public socket: Object) { 
+  public socket :any
+  constructor() { 
     this.socket = io();
   }
 }
