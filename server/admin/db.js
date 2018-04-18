d543bba04bd3af2cba1b0c149052ec3a18c3377c
Trung <lantrungsir28@gmail.com>
2018-04-18 13:58:17 +0700

    F

--- server/admin/db.js
+++ server/admin/db.js
@@ -1,4 +1,5 @@
 var db = require("./admin.js").database();
+var st = require("./admin.js").storage().bucket();
 module.exports = {
     saveData: function(path, data){
         db.ref(path).set(data);
@@ -18,14 +19,25 @@ module.exports = {
         })
     },
     listener : function(){
+        st.file('botty-green.jpg').makePublic().then(()=>{});
         db.ref("posts/num").set(0);
+        db.ref("posts/content/1").set({
+            "author" : "admin",
+            "message" :"Welcome to our new social website, GART members. I'm Botty Green, your virtual assistant and the admin of the website. Please feel free to use the service I offer you",
+            "time" :  new Date().toLocaleDateString()
+        })
         db.ref("posts/content").on("child_added", function(snapshot, prevKey){
+            db.ref("posts/content/"+snapshot.key +"/comments/content/1").set({
+                "author" : "admin",
+                "message" : "Look like you have posted a post. Well done !!_!!"
+            })
             db.ref("posts/num").once("value", function(snap){
                 var value = snap.val() + 1;
                 db.ref("posts/num").set(value);
                 db.ref("posts/content/"+snapshot.key+"/comments/num").set(0)
             })
             db.ref("posts/content/"+snapshot.key +"/comments/content").on("child_added", (snap,preK)=>{
+                
                 db.ref("posts/content/"+snapshot.key+"/comments/num").once("value", function(num){
                     var value = num.val() + 1;
                     db.ref("posts/content/"+snapshot.key+"/comments/num").set(value);
