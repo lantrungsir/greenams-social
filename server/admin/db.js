0bdbda76c68c90c576e0ca659a1a4897afab928a
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-20 18:23:21 +0700

    I am stronger

--- server/admin/db.js
+++ server/admin/db.js
@@ -24,16 +24,9 @@ module.exports = {
             db.ref("date/posts").once("value", function(date){
                 console.log(new Date(date.val()))
                 if(new Date().getSeconds() - new Date(date.val()).getSeconds() >= 32000000){
-                    db.ref("posts/content").once("value", function(data){
-                        for(key in data.val()){
-                            if(data.val().hasOwnProperty(key)){
-                                if(key !== snapshot.key){
-                                    db.ref("posts/content/"+key).set(null)
-                                }
-                            }
-                        }
-                    })
+                    db.ref("posts/content").set(null);
                     db.ref("date/posts").set(new Date().toDateString());
+                    db.ref("posts/content/1").set(snapshot.val());
                 }
             })
             
@@ -50,12 +43,6 @@ module.exports = {
                 })
             })
         })
-        db.ref("posts/content").on("child_removed", function(snapshot){
-            db.ref("posts/num").once("value", function(snap){
-                var value = snap.val() - 1;
-                db.ref("posts/num").set(value);
-            })
-        })
         db.ref("messages").on("child_added", function(category, nextkey){
             db.ref("messages/"+category.key).on("child_added",function(chatroom, nextkey){
                     db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/num").set(0)
@@ -66,30 +53,12 @@ module.exports = {
                         })
                         db.ref("date/messenger").once("value", function(date){
                             if(new Date(date.val()).getSeconds() + 630000 <= new Date().getSeconds()){
-                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/content").once("value", function(data){
-                                    for(key in data.val()){
-                                        if(data.val().hasOwnProperty(key)){
-                                            if(key !== message.key){
-                                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/content/"+key).set(null)
-                                            }
-                                        }
-                                    }
-                                })
+                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/content").set(null);
                                 db.ref("date/messenger").set(new Date().toDateString());
+                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/content/0").set(message.val());
                             }
                         })
                     })
-                    db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/content").on("child_removed", function(snapshot){
-                        db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/num").once("value", function(num){
-                            var value = num.val()-1;
-                            if(value < 0){
-                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/num").set(0)
-                            }
-                            else{
-                                db.ref("messages/"+ category.key +"/"+chatroom.key +"/messages/num").set(value)
-                            }
-                        })
-                    })  
             })
         })
     },
