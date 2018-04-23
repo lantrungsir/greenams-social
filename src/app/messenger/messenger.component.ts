4ed278a19e07b5be82a65efa148e11e3da479445
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-23 22:52:52 +0700

    I am stronger

--- src/app/messenger/messenger.component.ts
+++ src/app/messenger/messenger.component.ts
@@ -260,6 +260,7 @@ export class MessengerComponent implements OnInit {
         var id = $(this).id;
         console.log(id)
         var bool = (this.users[id].name.indexOf(searchVal) !== -1 || this.groups[id].name.indexOf(searchVal) !== -1)
+        console.log(bool)
         $(this).toggle(bool)
       })
     }
