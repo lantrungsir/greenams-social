b0d51c661b1340645dbd107863d9abf558b45a56
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-24 18:51:14 +0700

    I am stronger

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -186,4 +186,7 @@ export class DashboardComponent implements OnInit {
     this.userService.logout();
     this.router.navigate(['login'])
   }
+  scrollToSchedule(){
+    $('html').scrollTop($("#schedule").offset().top)
+  }
 }
