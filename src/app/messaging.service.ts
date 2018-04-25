f4f658981632cfe46b8ea810fec111a7ae2bb7bf
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-25 10:31:48 +0700

    I am stronger

--- src/app/messaging.service.ts
+++ src/app/messaging.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from "firebase"
 import { AuthHttp } from 'angular2-jwt';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'firebase/messaging';
 @Injectable()
 export class MessagingService {
   currentMessage = new BehaviorSubject(null);
