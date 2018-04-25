aa3da2d0ff80f9165a1162af903d440b5fbc813d
Nguyen Lan <lantrungsir28@gmail.com>
2018-04-25 11:39:23 +0700

    I am stronger

--- src/app/messaging.service.ts
+++ src/app/messaging.service.ts
@@ -14,15 +14,6 @@ export class MessagingService {
       storageBucket: "free-schedule.appspot.com",
       messagingSenderId: "410685537662"
     })
-     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/noti-sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    }
    }
   getPermission(){
     firebase.messaging().requestPermission()
