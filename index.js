54d974b1db6ffc9db43593e05aea078a0a2a88b5
Nguyen Lan <lantrungsir28@gmail.com>
2018-05-01 23:22:46 +0700

    I am stronger

--- index.js
+++ index.js
@@ -250,8 +250,8 @@ io.on("connection", function(socket){
             }
             else{
                 db.ref("users/"+data.recipient).once("value", function(snapshot){
-                    if(!snapshot.child("realtime").existed()){
-                        if(snapshot.child("fcm-token").existed()){
+                    if(!snapshot.child("realtime").exists()){
+                        if(snapshot.child("fcm-token").exists()){
                             var tok = snapshot.child("fcm-token").val();
                             database.getData("users/"+ data.sender).then((userData)=>{
                                 msg.sendToDevice(tok, {
