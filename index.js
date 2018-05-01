0cc83abc50454922df16416e4f360133a3034d3c
Nguyen Lan <lantrungsir28@gmail.com>
2018-05-01 17:03:27 +0700

    I am stronger

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ io.on("connection", function(socket){
                                 }
                             }
                         }
+                        database.saveData("meets/"+key, null);
                     })
                 }
                 if(time <0){
